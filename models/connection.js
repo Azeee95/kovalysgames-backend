27f809c49a4b783e6b3c062398639c2ca0147997
Kodzo <Kodzo@kovalys-cloud.com>
2023-09-30 09:29:26 +0200

    Commit test modif connectionString

--- models/connection.js
+++ models/connection.js
@@ -2,7 +2,10 @@ require ('dotenv').config();
 
 const mongoose = require('mongoose');
 
-const connectionString = process.env.connectionString;
+// const connectionString = process.env.connectionString;
+
+
+const connectionString = 'mongodb+srv://kodzo:testtest@kodzo.u7u0jcl.mongodb.net/kovalysgames'
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
  .then(() => console.log('Database connected'))
